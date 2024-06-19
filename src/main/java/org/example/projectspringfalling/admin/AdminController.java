ab10d8e27c67e706e4e5b9bb30859a5b45686011
coderyu5523 <coderyu5523@naver.com>
2024-06-19 19:17:20 +0900

    admin album detail

--- src/main/java/org/example/projectspringfalling/admin/AdminController.java
+++ src/main/java/org/example/projectspringfalling/admin/AdminController.java
@@ -3,11 +3,19 @@ package org.example.projectspringfalling.admin;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
 public class AdminController {
     private final AdminService adminService;
     private final HttpSession session;
+
+    // 관리자 앨범 상세보기
+    @GetMapping("/admin/albums/{id}")
+    public String albumDetail(@PathVariable Integer id) {
+        return "admin/admin-album-detail";
+    }
 
 }
