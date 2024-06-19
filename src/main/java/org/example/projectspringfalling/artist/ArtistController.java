9c8c3dff6df51e6ce25df139c6e592dec6290ee0
coderyu5523 <coderyu5523@naver.com>
2024-06-19 16:41:22 +0900

    artist detail

--- src/main/java/org/example/projectspringfalling/artist/ArtistController.java
+++ src/main/java/org/example/projectspringfalling/artist/ArtistController.java
@@ -3,11 +3,18 @@ package org.example.projectspringfalling.artist;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
 public class ArtistController {
     private final ArtistService artistService;
     private final HttpSession session;
+
+    @GetMapping("/artist/{id}")
+    public String artistDetail(@PathVariable Integer id) {
+        return "artist/artist-detail";
+    }
 
 }
