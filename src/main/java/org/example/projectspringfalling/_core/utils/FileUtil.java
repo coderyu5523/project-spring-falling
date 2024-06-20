d4ed0cf8d041cd63a4589d072e5e7ce59296bcaa
coderyu5523 <coderyu5523@naver.com>
2024-06-20 11:33:09 +0900

    [fix] fileUtil

--- src/main/java/org/example/projectspringfalling/_core/utils/FileUtil.java
+++ src/main/java/org/example/projectspringfalling/_core/utils/FileUtil.java
@@ -3,7 +3,11 @@ package org.example.projectspringfalling._core.utils;
 import org.apache.commons.io.IOUtils;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.io.*;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Base64;
@@ -11,64 +15,130 @@ import java.util.UUID;
 
 public class FileUtil {
 
-    // 파일을 Base64로 디코딩하여 저장하고 UUID 파일명을 반환하는 메서드
-    public static String decodeAsUUID(String fileData, String extension) throws IOException {
-        byte[] data = Base64.getDecoder().decode(fileData);
-        String fileUUID = UUID.randomUUID() + "." + extension;
-        String filePath = "./upload/" + fileUUID;
+    public static String decodeAsUUID(String img) throws IOException {
+        // 이미지를 디코딩하여 저장하고 파일명 생성
+        byte[] imageData = Base64.getDecoder().decode(img);
+        String imgUUID = UUID.randomUUID() + ".jpg";
+        String imagePath = "./upload/" + imgUUID;
 
-        // 파일로 저장
-        File file = new File(filePath);
-        try (OutputStream outputStream = new FileOutputStream(file)) {
-            outputStream.write(data);
-        }
+        // 이미지 파일로 저장
+        File file = new File(imagePath);
+        OutputStream outputStream = new FileOutputStream(file);
+        outputStream.write(imageData);
+        outputStream.close();
 
-        return fileUUID;
+        return imgUUID;
     }
 
-    // 파일을 저장하고 UUID 파일명을 반환하는 메서드
-    public static String storeFile(MultipartFile file) throws IOException {
-        if (file.isEmpty()) {
-            throw new IOException("파일이 비어있습니다.");
+    /**
+     * 이미지를 지정된 너비와 높이로 조정하고 저장하는 메서드
+     *
+     * @param errorMsg     에러 메시지
+     * @param img          이미지 파일명
+     * @param imageFile    조정할 이미지 파일
+     * @param targetWidth  원하는 이미지의 너비
+     * @param targetHeight 원하는 이미지의 높이
+     * @return 조정된 이미지의 UUID
+     * @throws IOException 이미지 조정 중 발생한 IO 예외
+     */
+    public static String imgResizedAndDownloadAndUUID(String errorMsg, String img, MultipartFile imageFile, int targetWidth, int targetHeight) throws IOException {
+        if (imageFile == null || imageFile.isEmpty()) {
+            throw new Exception400(errorMsg + "가 없습니다.");
         }
 
-        String originalFileName = file.getOriginalFilename();
-        String fileUUID = UUID.randomUUID() + "_" + originalFileName;
-        Path filePath = Paths.get("./upload/" + fileUUID);
+        // MultipartFile을 BufferedImage로 변환
+        BufferedImage originalImage = ImageIO.read(new ByteArrayInputStream(imageFile.getBytes()));
 
-        // 디렉토리가 없으면 생성
-        File uploadDir = new File("./upload/");
-        if (!uploadDir.exists()) {
-            boolean created = uploadDir.mkdirs();
-            if (!created) {
-                throw new IOException("업로드 디렉토리를 생성할 수 없습니다.");
-            }
+        int originalWidth = originalImage.getWidth();
+        int originalHeight = originalImage.getHeight();
+
+        // 비율을 유지하면서 새 크기 계산
+        int newWidth = originalWidth;
+        int newHeight = originalHeight;
+
+        if (originalWidth > targetWidth) {
+            newWidth = targetWidth;
+            newHeight = (newWidth * originalHeight) / originalWidth;
         }
 
-        // 파일 저장
-        file.transferTo(filePath.toFile());
+        if (newHeight > targetHeight) {
+            newHeight = targetHeight;
+            newWidth = (newHeight * originalWidth) / originalHeight;
+        }
 
-        return fileUUID;
+        // 이미지를 조정된 크기로 리사이즈
+        Image resultingImage = originalImage.getScaledInstance(newWidth, newHeight, Image.SCALE_SMOOTH);
+        BufferedImage outputImage = new BufferedImage(newWidth, newHeight, BufferedImage.TYPE_INT_RGB);
+        Graphics2D g2d = outputImage.createGraphics();
+        g2d.drawImage(resultingImage, 0, 0, null);
+        g2d.dispose();
+
+        // 이미지 파일로 저장
+        String imgUUID = UUID.randomUUID() + "_" + img;
+        Path imgPaths = Paths.get("./upload/" + imgUUID);
+        ImageIO.write(outputImage, "jpg", Files.newOutputStream(imgPaths));
+
+        return imgUUID;
     }
 
-    // 파일을 Base64로 인코딩하는 메서드
-    public static String encode(String fileName) throws IOException {
-        String filePath = "./upload/" + fileName;
-        File file = new File(filePath);
+
+    /**
+     * 이미지를 지정된 너비와 높이로 조정하는 메서드
+     *
+     * @param imageFile    조정할 이미지 파일
+     * @param targetWidth  원하는 이미지의 너비
+     * @param targetHeight 원하는 이미지의 높이
+     * @return 조정된 이미지를 바이트 배열로 반환
+     * @throws IOException 이미지 조정 중 발생한 IO 예외
+     */
+    public static byte[] imgResized(MultipartFile imageFile, int targetWidth, int targetHeight) throws IOException {
+        // MultipartFile을 BufferedImage로 변환
+        BufferedImage originalImage = ImageIO.read(new ByteArrayInputStream(imageFile.getBytes()));
+
+        // 이미지를 지정된 크기로 조정
+        Image resultingImage = originalImage.getScaledInstance(targetWidth, targetHeight, Image.SCALE_SMOOTH);
+
+        // 조정된 이미지를 BufferedImage로 다시 변환
+        BufferedImage outputImage = new BufferedImage(targetWidth, targetHeight, BufferedImage.TYPE_INT_RGB);
+        outputImage.getGraphics().drawImage(resultingImage, 0, 0, null);
+
+        // 조정된 이미지를 바이트 배열로 변환하여 반환
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        ImageIO.write(outputImage, "jpg", outputStream);
+        return outputStream.toByteArray();
+    }
+
+    /**
+     * 이미지 파일을 Base64로 인코딩하는 메서드
+     *
+     * @param img 이미지 파일명
+     * @return Base64로 인코딩된 이미지 데이터
+     * @throws IOException 이미지 파일을 읽어오는 도중 발생한 예외
+     */
+    public static String encode(String img) throws IOException {
+        String imagePath = "./upload/" + img;
+        File file = new File(imagePath);
 
         // 파일이 존재하면 읽어들임
         if (file.exists()) {
-            try (InputStream inputStream = new FileInputStream(file)) {
-                byte[] fileData = IOUtils.toByteArray(inputStream);
-                return Base64.getEncoder().encodeToString(fileData);
-            }
+            InputStream inputStream = new FileInputStream(file);
+            byte[] imageData = IOUtils.toByteArray(inputStream);
+            inputStream.close();
+
+            // 이미지를 Base64로 인코딩하여 직접 반환
+            return Base64.getEncoder().encodeToString(imageData);
         } else {
-            throw new FileNotFoundException("파일을 찾을 수 없습니다: " + filePath);
+            throw new FileNotFoundException("파일을 찾을 수 없습니다: " + imagePath);
         }
     }
 
-    // 파일의 UUID를 생성하는 메서드
-    public static String generateUUIDFileName(String fileName) {
-        return UUID.randomUUID() + "_" + fileName;
+    /**
+     * 이미지 파일의 UUID를 생성하는 메서드
+     *
+     * @param img 이미지 파일명
+     * @return 생성된 UUID
+     */
+    public static String imgUUID(String img) {
+        return UUID.randomUUID() + "_" + img;
     }
 }
