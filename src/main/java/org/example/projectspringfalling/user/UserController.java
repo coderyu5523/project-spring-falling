0897b8ff941dba1bf0d22e928a02e9618c81d528
minmeanmin <wed1604@naver.com>
2024-06-20 12:04:01 +0900

    temp

--- src/main/java/org/example/projectspringfalling/user/UserController.java
+++ src/main/java/org/example/projectspringfalling/user/UserController.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @RequiredArgsConstructor
 @Controller
@@ -17,20 +18,31 @@ public class UserController {
     public String joinSection() {
         return "user/join-section";
     }
+
     // 회원가입 페이지
     @GetMapping("/join-form")
     public String joinForm() {
         return "user/join-form";
     }
+
     // 로그인 페이지
     @GetMapping("/login-form")
     public String loginForm() {
         return "user/login-form";
     }
+
     // 마이페이지
     @GetMapping("/profile")
     public String profile() {
         return "user/profile-password";
 //        return "user/profile-phone";
     }
+
+    @GetMapping("/redis/test")
+    public @ResponseBody String redisTest() {
+        User sessionUser = (User) session.getAttribute("sessionUser");
+        System.out.println("username : " + sessionUser.getEmail());
+        return "redis test";
+    }
+
 }
--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -1,1 +1,5 @@
-INSERT INTO artist_tb (name, artist_type, artist_img,created_at) VALUES ('노을', 'Solo', 'artist_image.jpg',now());
+INSERT INTO artist_tb (name, artist_type, artist_img, created_at)
+VALUES ('노을', 'Solo', 'artist_image.jpg', now());
+
+insert into user_tb (email, password, phone, birth, provider, created_at)
+VALUES ('ssar@nate.com', '1234', '01012341234', '1999-01-01', 'KAKAO', now());
