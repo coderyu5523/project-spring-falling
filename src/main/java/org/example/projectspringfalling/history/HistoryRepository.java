dcbc1e005d610837357cb43bf0af8e355fa59aee
kjs <zeeq125@gmail.com>
2024-06-28 15:17:15 +0900

    앨범 좋아요

--- src/main/java/org/example/projectspringfalling/history/HistoryRepository.java
+++ src/main/java/org/example/projectspringfalling/history/HistoryRepository.java
@@ -9,6 +9,6 @@ import java.util.List;
 public interface HistoryRepository extends JpaRepository<History, Integer> {
 
     // 보관함 - 최근 감상
-    @Query("SELECT new org.example.projectspringfalling.RestAPI.RestResponse$RecentSongs(s.id, a.id, a.albumImg, s.title, a.title, ar.name) FROM History h JOIN h.song s JOIN s.album a JOIN s.artist ar WHERE h.user.id = :userId")
+    @Query("SELECT new org.example.projectspringfalling.RestAPI.RestResponse$RecentSongs(s.id, a.id, a.albumImg, s.title, a.title, ar.name) FROM History h JOIN h.song s JOIN s.album a JOIN s.artist ar WHERE h.user.id = :userId ORDER BY h.createdAt DESC ")
     List<RestResponse.RecentSongs> findAllRecentSongs(Integer userId);
 }
--- src/main/resources/static/js/album-detail.js
+++ src/main/resources/static/js/album-detail.js
@@ -1,19 +1,24 @@
 $(document).ready(function () {
-    let albumAbout = $("#album-about");
-    let albumSongList = $("#album-song-list");
-    let albumAboutTab = $(".album-list-album-detail");
-    let albumSongs = $(".album-list-album-songs");
+    let albumAbout = $("#album-about"); // 앨범 상세정보
+    let albumSongList = $("#album-song-list"); // 앨범 수록곡
+    let albumAboutTab = $(".album-list-album-detail"); // 앨범 상세정보 탭
+    let albumSongs = $(".album-list-album-songs"); // 앨범 수록곡 탭
+    let albumLike = $("#album-like"); // 앨범 좋아요 여부 (boolean)
+    let albumId = $("#album-id"); // 앨범 id
 
+    // 탭을 바꿨을 때 선택된 탭에 따른 내용 보여지게 하기
     function appearContent(tab) {
         tab.siblings().css("display", "none");
         tab.css("display", "block");
     }
 
+    // 선택된 탭 스타일 변경(클래스 추가, 제거)
     function selectTab(tab) {
         tab.siblings().removeClass("album-selected-tab");
         tab.addClass("album-selected-tab");
     }
 
+    // 탭 버튼에 함수 추가
     $(".album-list-tab-button").on("click", "button", function () {
         switch ($(this).data("tab")) {
             case "album-about":
@@ -26,4 +31,42 @@ $(document).ready(function () {
                 break;
         }
     });
+
+    // 좋아요 나타나게 하기(true, false)
+    function likeButtonAppear(value) {
+        switch (value.val()) {
+            case 'true':
+                $(value).siblings().remove(".like-icons");
+                $(value).after("<a class='like-icons' href='#'><i id='liked-album' class='album-list-btn-add-like fas fa-heart'></i></a>");
+                doLike("DELETE", "좋아요 취소 실패", "false");
+                break;
+            case 'false':
+                $(value).siblings().remove(".like-icons");
+                $(value).after("<a class='like-icons' href='#'><i id='unliked-album' class='album-list-btn-add-like fa-regular fa-heart'></i></a>");
+                doLike("POST", "좋아요 실패", "true");
+                break;
+        }
+    }
+
+    // 좋아요, 좋아요 취소
+    function doLike(method, failMsg, value) {
+        $(".like-icons").click(function () {
+            $.ajax({
+                url: "/albums/like",
+                method: method,
+                contentType: "application/json",
+                data: JSON.stringify(albumId.val()),
+                success: function () {
+                    albumLike.val(value);
+                    likeButtonAppear(albumLike);
+                },
+                error: function () {
+                    alert(failMsg);
+                }
+            })
+        });
+    }
+
+    // 좋아요 초기상태
+    likeButtonAppear(albumLike);
 });
