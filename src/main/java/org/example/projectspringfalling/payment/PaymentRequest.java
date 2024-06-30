bc60ccfd5b2482875e3c5cbc4c88a1206491e14c
minmeanmin <wed1604@naver.com>
2024-06-30 13:09:50 +0900

    temp

--- src/main/java/org/example/projectspringfalling/payment/PaymentRequest.java
+++ src/main/java/org/example/projectspringfalling/payment/PaymentRequest.java
@@ -1,4 +1,15 @@
 package org.example.projectspringfalling.payment;
 
+import lombok.Data;
+
 public class PaymentRequest {
+    // 결제 DTO
+    @Data
+    public static class PaymentDTO {
+        private String impUid; // 결제 고유번호
+        private String merchantUid; // 주문번호
+        private String subscriptionPk; // 이용권 id
+        private String name; // 상품 이름
+        private String amount; // 결제 금액
+    }
 }
